7fab9abf1505c000d4e879bb4dc7ad98b16ecee7
Francisco Rocha <fa676533@gmail.com>
2026-06-05 19:02:00 -0600

    feat/formulario-creado

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+
+export default function Header() {
+
+    return (
+        <h1 className="font-karla font-bold text-3xl text-left text-grey-900">
+            Contact Us
+        </h1>
+    )
+
+}
